758d8c7c50f893841600b30f076d103385c3b152
HotJuly <949588380@qq.com>
2022-11-16 10:53:46 +0800

    32.Vue-API(选项数据)

---
+++ 02.代码/3.vue2_api/src-3.全局API/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <h2>pageX:{{ pageX }}</h2>
+    <h2>pageY:{{ pageY }}</h2>
+    <h2>num:{{ num }}</h2>
+  </div>
+</template>
+
+<script>
+import moveMixin from '@/mixins/moveMixin'
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      pageX:200,
+      num:999
+    }
+  },
+  methods:{
+    moveHandler(event){
+          // console.log(event)
+          const {clientX,clientY} = event;
+          this.pageX = clientX;
+          this.pageY = clientY;
+          console.log('组件的methods方法')
+        }
+  },
+  mixins:[moveMixin]
+}
+</script>
+
+<style>
+
+</style>
