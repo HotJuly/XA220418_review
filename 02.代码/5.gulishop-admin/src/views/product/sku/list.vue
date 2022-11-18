356e509f31f554d443e2dae90ee0de6ca7fe52d0
HotJuly <949588380@qq.com>
2022-11-18 16:59:55 +0800

    58.scoped原理讲解

---
+++ 02.代码/5.gulishop-admin/src/views/product/sku/list.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="sku">
+      <h1>喜欢我的请亮灯:</h1>
+      <h2>刘亦菲</h2>
+      <Child/>
+  </div>
+</template>
+
+<script>
+import Child from './test/Child.vue';
+export default {
+    name:"SKU",
+    components:{
+      Child
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+#sku{
+  // h2{
+  //   color:red;
+  //   font-size:30px;
+  // }
+  ::v-deep h2{
+    color:red;
+    font-size:30px;
+  }
+  
+  // vue3版本写法
+  // ::deep(h2){
+  //   color:red;
+  //   font-size:30px;
+  // }
+}
+
+</style>
---
+++ 02.代码/5.gulishop-admin/src/views/product/sku/test/Child.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <h2>邱淑贞</h2>
+      <h2>王祖贤</h2>
+    </div>
+    
+    <!-- <h2>邱淑贞</h2> -->
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
